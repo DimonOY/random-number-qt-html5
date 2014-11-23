8f383e9f12d03ceeeb46f4a90e72cf1f689961e7
Zhi <yfwz100@yeah.net>
2014-11-23 16:28:32 +0800

    The initial work.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QtGui/QApplication>
+#include "html5applicationviewer.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    Html5ApplicationViewer viewer;
+    viewer.setOrientation(Html5ApplicationViewer::ScreenOrientationAuto);
+    viewer.showExpanded();
+    viewer.setFixedSize(1024, 650);
+    viewer.loadFile(QLatin1String("html/index.html"));
+
+    return app.exec();
+}
